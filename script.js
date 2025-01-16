6571c3fcbaa248cde0f82bea55931c455c6da1b2
bikash5750 <gbikash5750@gmail.com>
2025-01-16 17:46:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const incidentForm = document.getElementById("incidentForm");
+  const resourceForm = document.getElementById("resourceForm");
+  const incidentList = document.querySelector("#incidentList ul");
+  const resourceList = document.querySelector("#resourceList ul");
+
+
+  incidentForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const incidentType = document.getElementById("incidentType").value;
+      const location = document.getElementById("location").value;
+
+      const li = document.createElement("li");
+      li.textContent = `Type: ${incidentType}, Location: ${location}`;
+      incidentList.appendChild(li);
+
+      incidentForm.reset();
+  });
+
+  resourceForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const resourceType = document.getElementById("resourceType").value;
+      const quantity = document.getElementById("quantity").value;
+
+      const li = document.createElement("li");
+      li.textContent = `Resource: ${resourceType}, Quantity: ${quantity}`;
+      resourceList.appendChild(li);
+
+      resourceForm.reset();
+  });
+});
